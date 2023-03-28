979d37d90af60f0f30fdf7afda71213afe11ae7e
Pedro Macedo <pedro.macedo@italtel.com>
2023-03-27 23:27:22 -0300

    Just a few tests on the rollback tool

--- vlan.py
+++ vlan.py
@@ -8,7 +8,7 @@ from nornir_utils.plugins.functions import print_result
 import os
 
 from doc_nornir_files.processors import PrintResult, SaveResultToDict
-from utils.rollback import rollback_point,rollback
+from utils.rollback import rollback_point,rollback,rollback_diff
 
 nr = InitNornir(config_file="config.yaml")
 switch = nr.filter(F(groups__contains="switch"))
@@ -33,25 +33,29 @@ def add_vlan(task: Task, vlan_id: int, vlan_name: str) -> Result:
     )
     
 def add_itf_to_vlan(task: Task, vlan_id: int, interface_id: str) -> Result:
-    
-    task.run(task=netmiko_send_config,config_commands=[f"interface FastEthernet {interface_id}",f"switchport mode access",f"switchport access vlan {vlan_id}"])
+    commands = [f"interface FastEthernet {interface_id}",f"switchport mode access",f"switchport access vlan {vlan_id}"]
+    task.run(task=netmiko_send_config,config_commands=commands)
     return Result (
         host = task.host,
-        result=f"Vlan {vlan_id} added to {task.host} interface {interface_id}"
+        result=commands
     )
     
 def full_vlan(task: Task,vlan_id: int, interface_id: str,vlan_name: str) -> Result:
     
     rollback_point(task=task,service_name=service_name,vlan_id=vlan_id,interface_id=interface_id,devices_object=switch,devices=devices)
     
-    task.run(task=add_vlan,vlan_id=vlan_id,vlan_name=vlan_name)
+    #task.run(task=add_vlan,vlan_id=vlan_id,vlan_name=vlan_name)
     task.run(task=add_itf_to_vlan,vlan_id=vlan_id,interface_id=interface_id)
+    
+    rollback_diff(interface_id="1/2",service_id=1,devices=devices,devices_object=switch)
     
 
 rollback(devices_object=switch,devices=devices,service_id=1)
 
+
+
 service_name = 'vlan'
-#result = switch.run(name=service_name,task=full_vlan,vlan_id=200,interface_id="1/2",vlan_name="Teste")
-""" print(result)
-print(result['sw1'][0]) """
+""" result = switch.run(name=service_name,task=full_vlan,vlan_id=300,interface_id="1/2",vlan_name="ChangeTeste")
+print(result)
+ """
 
