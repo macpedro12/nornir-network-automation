0ad640586ad80f19ee0fabaab1b67df2253ab39c
Pedro Macedo <pedro.macedo@italtel.com>
2023-10-26 01:09:23 -0300

    Moved the database script

--- python/src/database/database.py
+++ python/src/database/database.py
@@ -2,22 +2,28 @@ import os
 import shutil
 import psycopg2
 
+from database.service_object import Service
+
 def database_connection():
     
     conn = psycopg2.connect( 
         dbname='postgres', 
         user='admin', 
         password='admin123', 
-        host="localhost", 
-        port="5432" 
+        host='localhost', 
+        port='5432' 
     ) 
 
     cur = conn.cursor()
         
-    cur.execute("INSERT INTO service (service_id,service_name,applied_config,initial_config,status) VALUES (%s, %s, %s, %s, %s)",
-                (1, "teste-service","ip 19.1.1.1","no ip","applied"))
+    cur.execute('INSERT INTO service (service_id,service_name,applied_config,initial_config,status) VALUES (%s,%s,%s,%s,%s)',
+                (1,'teste-service','ip 19.1.1.1','no ip','applied'))
     
-    cur.execute("SELECT * FROM service")
+    """     INSERT INTO service
+    (service_id,service_name,applied_config,initial_config,status)
+    VALUES(2,'teste-service2','ip 19.1.1.12','no ip2','applied2');
+    """
+    cur.execute('SELECT * FROM service')
     rows = cur.fetchall() 
     
     print(rows)
@@ -25,7 +31,6 @@ def database_connection():
     cur.close()
     conn.close()
 
-database_connection()  
 # This script will be used to create the database
 # Another script will handle the rollback
 def database_insert(id,device,service_config):
