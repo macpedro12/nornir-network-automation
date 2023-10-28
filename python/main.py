3604b8cbee946eeeb8de4f74a295bdd4314a6a8c
Pedro Macedo <pedro.macedo@italtel.com>
2023-10-28 17:26:47 -0300

    Added rollback call

--- python/main.py
+++ python/main.py
@@ -1,15 +1,21 @@
 import sys
 from importlib import import_module
+from nornir import InitNornir
 
 from src.database import database
 from src.service_provider.service_provider import create_service
+from src.rollback.rollback import rollback as rb
 
 def new_service():
     
     service_info = create_service()
     database.database_insert(service_info)
 
+def rollback(id):
     
+    rollback_status = rb(id)
+    database.database_update(id,'status',rollback_status)
+   
 if __name__ == '__main__':
     args = sys.argv
     # args[0] = current file
