a3d1cd77ff11ad8b9231c96fdf6ca4455f05545f
Pedro Macedo <pedro.macedo@italtel.com>
2023-10-27 23:54:17 -0300

    Added exception handler in case the script is executed without the first argument

--- python/main.py
+++ python/main.py
@@ -2,7 +2,6 @@ import sys
 from importlib import import_module
 
 from src.database import database
-from src.database import get_device_config
 from src.service_provider.service_provider import create_service
 
 def new_service():
@@ -11,10 +10,12 @@ def new_service():
     database.database_insert(service_info)
 
     
-    
 if __name__ == '__main__':
     args = sys.argv
     # args[0] = current file
     # args[1] = function name
     # args[2:] = function args : (*unpacked)
-    globals()[args[1]](*args[2:])
+    try:
+        globals()[args[1]](*args[2:])
+    except IndexError:
+        print('Please, select a function, ex: new_service')
