1659a4a2edef3b516caf36e700bd7c1cfc294c44
Pedro Macedo <pedro.macedo@italtel.com>
2023-10-29 19:45:42 -0300

    Edited service selection when creating a new service

--- python/main.py
+++ python/main.py
@@ -1,15 +1,22 @@
 import sys
 from importlib import import_module
 from nornir import InitNornir
+from os import listdir
 
 from src.database import database
 from src.service_provider.service_provider import create_service
 from src.rollback.rollback import rollback as rb
 
-def new_service():
+def new_service(service):
     
-    service_info = create_service()
-    database.database_insert(service_info)
+    services_list = listdir("./python/src/service_provider/services/")
+    service = str(service).replace(" ","").lower()
+    
+    if service in services_list:
+        service_info = create_service(service)
+        database.database_insert(service_info)
+    else:
+        print(f"Service doesn't exist, follow the services available:\n", [service for service in services_list])
 
 def rollback(id):
     
@@ -24,4 +31,4 @@ if __name__ == '__main__':
     try:
         globals()[args[1]](*args[2:])
     except IndexError:
-        print('Please, select a function, ex: new_service')
+        print('Please, select a function, ex: new_service "service"')
