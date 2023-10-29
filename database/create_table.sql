c3d02e568c0c6ae64b3176d110ba08bd5fcbfd69
Pedro Macedo <pedro.macedo@italtel.com>
2023-10-29 19:45:06 -0300

    Added new collumns to the services table and created the device table

--- database/create_table.sql
+++ database/create_table.sql
@@ -1,8 +1,19 @@
-CREATE TABLE IF NOT EXISTS service (
+CREATE TABLE IF NOT EXISTS services (
     service_id INT NOT NULL,
+    edit_id INT NOT NULL,
+    service_applied TEXT NOT NULL,
     service_name TEXT NOT NULL,
     applied_config TEXT,
     initial_config TEXT,
     status TEXT,
     PRIMARY KEY (service_id)
+);
+
+CREATE TABLE IF NOT EXISTS devices (
+    hostname TEXT NOT NULL,
+    ip TEXT NOT NULL,
+    running_config TEXT,
+    applied_services TEXT,
+    status TEXT,
+    PRIMARY KEY (hostname)
 );
