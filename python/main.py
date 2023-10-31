d1a1c064ea92d03ccea0de8f1c317a28fc2d3b9a
Pedro Macedo <pedro.macedo@italtel.com>
2023-10-30 23:45:25 -0300

    Added new exception handler as changed function import name to avoid errors

--- python/main.py
+++ python/main.py
@@ -4,16 +4,16 @@ from nornir import InitNornir
 from os import listdir
 
 from src.database import database
-from src.service_provider.service_provider import create_service
+from src.service_provider.service_provider import create_service as cs
 from src.rollback.rollback import rollback as rb
 
 def new_service(service):
     
     services_list = listdir("./python/src/service_provider/services/")
     service = str(service).replace(" ","").lower()
-    
+        
     if service in services_list:
-        service_info = create_service(service)
+        service_info = cs(service)
         database.database_insert(service_info)
     else:
         print(f"Service doesn't exist, follow the services available:\n", [service for service in services_list])
@@ -32,3 +32,5 @@ if __name__ == '__main__':
         globals()[args[1]](*args[2:])
     except IndexError:
         print('Please, select a function, ex: new_service "service"')
+    except KeyError:
+        print('Function do not exist, to create a new service use: new_service "service", to execute a rollback on service use: rollback id')
